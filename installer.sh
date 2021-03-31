49e2d1e664acc928627d12c251a4ead71d7b00e3
Nick-vdM <nick.jvdmerwe@gmail.com>
2021-03-31 13:09:06 +1000

    Updating installer.sh to install pyinstaller

--- installer.sh
+++ installer.sh
@@ -1,3 +1,17 @@
-cp dist ~/.maths_test_tools -r
+#!/usr/bin/env bash
+if pip3 list | grep -F pyinstaller 2>&1 >/dev/null; echo $?;
+    then
+        echo "Pyinstaller is already installed";
+    else
+        pip3 install pyinstaller;
+fi
+
+pyinstaller src/addition.py --specpath spec --onefile
+pyinstaller src/subtraction.py --specpath spec --onefile
+pyinstaller src/square.py --specpath spec --onefile
+pyinstaller src/complement.py --specpath spec --onefile
+pyinstaller src/multiply.py --specpath spec --onefile
+
+cp -r dist ~/.maths_test_tools
 export PATH=$PATH:~/.maths_test_tools
 echo -e "export PATH=$PATH:~/.maths_test_tools" >> ~/.bashrc
